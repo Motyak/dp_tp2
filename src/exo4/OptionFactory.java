306b64577679577be0d7a03e3171e29d1be666f5
Tommy Anfosso <anfossotommy@hotmail.fr>
2020-11-04 15:35:11 +0100

    hashmap de ces morts

--- src/exo4/OptionFactory.java
+++ src/exo4/OptionFactory.java
@@ -1,7 +1,20 @@
 package exo4;
 
+import java.util.HashMap;
+
 public class OptionFactory {
+	
+	private HashMap<String, Option> cache;
+	
+	public OptionFactory() {
+		this.cache = new HashMap<String, Option>();
+	}
+	
 	public Option createOption(String nom) {
-		return new Option(nom);
+		if(cache.containsKey(nom))
+			return this.cache.get(nom);
+		Option option = new Option(nom);
+		this.cache.put(nom, option);
+		return option;
 	}
 }
